97ccda8c436482083b2945490440f8b847f6fcbf
招牌疯子 <zp@buaa.us>
2016-01-15 14:50:06 +0800

    update fish config

--- config.fish
+++ config.fish
@@ -1,19 +1,11 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/zippo/.local/share/omf"
 
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/zippo/.config/omf"
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Custom plugins and themes may be added to ~/.oh-my-fish/custom
-# Plugins and themes can be found at https://github.com/oh-my-fish/
-Theme "robbyrussell"
-Plugin "theme"
-Plugin "brew"
-Plugin "git-flow"
-Plugin "gi"
+source $OMF_PATH/init.fish
 
 # Alias
 alias gitlog "git log --graph --pretty=format:'%C(yellow)%h%Creset%C(blue)%d%Creset %C(white bold)%s%Creset %C(white dim)(by %an %ar)%Creset'"
@@ -21,7 +13,13 @@ alias gits "git status"
 alias goget "gopm get -g -v"
 alias gitref "git remote update origin --prune"
 alias htop "sudo htop"
-alias pipup "pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+alias pipup "pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+alias pip3up "pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
+alias tailapi "multitail -s 2 -l \"ssh ol27 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol28 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol29 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol30 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\""
+alias tailuploader "multitail -s 2 -l \"ssh ol164 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol165 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol166 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol167 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol168 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol169 'tail -f /opt/tiger/mimg/logs/uploader.err'\""
+alias tailold "multitail -s 2 -l \"ssh ol12 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol13 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol14 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol31 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol32 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol33 'tail -f /opt/tiger/mimg/logs/uploader.err'\""
+alias tailconverter "multitail -s 2 -l \"ssh ol164 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol165 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol166 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol167 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol168 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol169 'tail -f /opt/tiger/mimg/logs/converter.err'\""
+alias tailttuploader "multitail -s 2 -l \"ssh tt16 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt17 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt18 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt19 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\""
 
 # for c/c++ build env
 # set -x CXX /usr/local/bin/g++-4.9
@@ -29,15 +27,17 @@ alias pipup "pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 p
 # set -x CPP /usr/local/bin/cpp-4.9
 
 # set -x LD_LIBRARY_PATH "$HOME/develop/python/clib"
-set -x PYTHONPATH $HOME/develop/python/mylibs:$HOME/develop/python/memory-album
+set -x PYTHONPATH $HOME/develop/python/mylibs:$HOME/develop/python/mosaic-dashboard:$HOME/develop/python
+
+set -x OPENSSL_ROOT_DIR "/usr/local/opt/openssl"
 
 # Set PATH with /usr/local at first place
-set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x PATH $HOME/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 
 # for golang env
 set -x GOPATH $HOME/develop/GO
 set -x GOBIN $GOPATH/bin
-set -x GOROOT /usr/local/Cellar/go/1.4.2/libexec
+set -x GOROOT /usr/local/Cellar/go/1.5.3/libexec
 set -x PATH $GOROOT/bin $GOBIN $PATH
 
 # Set git_editor as the private vim
@@ -69,4 +69,3 @@ function fuck -d 'Correct your previous console command'
         history --delete $fucked_up_command
     end
 end
-
--- vimrc
+++ vimrc
@@ -200,6 +200,17 @@ let g:go_fmt_autosave                      = 1
 " let g:go_fmt_command                       = "goimports"
 
 """"""""""""""""""""""""""""""
+" jedi settings
+""""""""""""""""""""""""""""""
+let g:jedi#goto_command = "<leader>d"
+let g:jedi#goto_assignments_command = "<leader>g"
+let g:jedi#goto_definitions_command = ""
+let g:jedi#documentation_command = "K"
+let g:jedi#usages_command = "<leader>n"
+let g:jedi#completions_command = "<C-Space>"
+let g:jedi#rename_command = "<leader>r"
+
+""""""""""""""""""""""""""""""
 "neocomplete settings
 """"""""""""""""""""""""""""""
 let g:neocomplete#enable_at_startup        = 1
