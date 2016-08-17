2ec15e9611cf993444cbf89a1094f208b97f059e
招牌疯子 <zp@buaa.us>
2016-08-17 17:51:00 +0800

    update fish config

--- config.fish
+++ config.fish
@@ -1,8 +1,8 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/zippo/.local/share/omf"
+set -gx OMF_PATH $HOME/.local/share/omf
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/zippo/.config/omf"
+# set -gx OMF_CONFIG $HOME/.config/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
@@ -10,37 +10,34 @@ source $OMF_PATH/init.fish
 # Alias
 alias gitlog "git log --graph --pretty=format:'%C(yellow)%h%Creset%C(blue)%d%Creset %C(white bold)%s%Creset %C(white dim)(by %an %ar)%Creset'"
 alias gits "git status"
-alias goget "gopm get -g -v"
 alias gitref "git remote update origin --prune"
 alias htop "sudo htop"
-alias pipup "pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
-alias pip3up "pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
-alias tailapi "multitail -s 2 -l \"ssh ol27 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol28 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol29 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\" -l \"ssh ol30 'tail -f /opt/tiger/everphoto/logs/everphoto-api.err'\""
-alias tailuploader "multitail -s 2 -l \"ssh ol164 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol165 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol166 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol167 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol168 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol169 'tail -f /opt/tiger/mimg/logs/uploader.err'\""
-alias tailold "multitail -s 2 -l \"ssh ol12 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol13 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol14 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol31 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol32 'tail -f /opt/tiger/mimg/logs/uploader.err'\" -l \"ssh ol33 'tail -f /opt/tiger/mimg/logs/uploader.err'\""
-alias tailconverter "multitail -s 2 -l \"ssh ol164 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol165 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol166 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol167 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol168 'tail -f /opt/tiger/mimg/logs/converter.err'\" -l \"ssh ol169 'tail -f /opt/tiger/mimg/logs/converter.err'\""
-alias tailttuploader "multitail -s 2 -l \"ssh tt16 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt17 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt18 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\" -l \"ssh tt19 'tail -f /opt/tiger/mimg/logs/ttuploader.err'\""
+alias pipup "pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip install -U"
+alias pip3up "pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install -U"
+alias service "brew services"
 
 # for c/c++ build env
 # set -x CXX /usr/local/bin/g++-4.9
 # set -x CC /usr/local/bin/gcc-4.9
 # set -x CPP /usr/local/bin/cpp-4.9
+set -x PKG_CONFIG_PATH $HOME/local/libpng/lib/pkgconfig:$HOME/local/imagick/lib/pkgconfig
 
 # set -x LD_LIBRARY_PATH "$HOME/develop/python/clib"
-set -x PYTHONPATH $HOME/develop/python/mylibs:$HOME/develop/python/mosaic-dashboard:$HOME/develop/python:/Users/zippo/develop/toutiao
+set -x PYTHONPATH $HOME/develop/python/pylib:$HOME/develop/python/flask-admin-libs:$HOME/develop/python:$HOME/develop/toutiao
 
 set -x LDFLAGS -L/usr/local/opt/openssl/lib
 set -x CPPFLAGS -I/usr/local/opt/openssl/include
 set -x OPENSSL_ROOT_DIR "/usr/local/opt/openssl"
 
 # Set PATH with /usr/local at first place
-set -x PATH $HOME/bin $HOME/.cargo/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin
+set -x PATH $HOME/.pyenv/shims $HOME/bin $HOME/.cargo/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin
 
 # for golang env
-set -x GOPATH $HOME/develop/GO
+set -x GOPATH $HOME/develop/go
 set -x GOBIN $GOPATH/bin
 set -x GOROOT /usr/local/opt/go/libexec
 set -x PATH $GOROOT/bin $GOBIN $PATH
+set -x CGO_ENABLED 0
 
 # for rust env
 set -x RUST_SRC_PATH $HOME/develop/rust/rust-lang/src
